465b0adf464ff0d9f01ebd10273f4d9f15296467
richard <rich.290401@gmail.com>
2023-07-25 23:24:24 +0200

    WS-10 Split admin to partials

--- Manager/Home/urls.py
+++ Manager/Home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'$', views.manager_index, name='manager-index'),
-    url(r'test/$', views.new_manager_index, name='new-manager-index'),
 ]
--- Manager/Home/views.py
+++ Manager/Home/views.py
@@ -5,7 +5,3 @@ from core.Utils.Access.decorators import manager_required
 @manager_required
 def manager_index(request):
     return render(request, 'Manager/manager_index.html')
-
-
-def new_manager_index(request):
-    return render(request, 'Manager/new_manager_index.html')
