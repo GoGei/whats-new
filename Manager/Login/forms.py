7eba7506e712b5eaf969ad8da396149d79bac415
richard <rich.290401@gmail.com>
2023-08-02 23:04:49 +0200

    WS-16: - Added new login page to manager part

--- Manager/Login/forms.py
+++ Manager/Login/forms.py
@@ -5,9 +5,11 @@ from django.utils.translation import ugettext as _
 class ManagerLoginForm(forms.Form):
     email = forms.EmailField(label=_('Email address'),
                              widget=forms.TextInput({'autofocus': 'autofocus',
+                                                     'class': 'form-control form-control-lg',
                                                      'placeholder': _('Email address')}))
     password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput({'placeholder': _('Password')}))
+                               widget=forms.PasswordInput({'class': 'form-control form-control-lg',
+                                                           'placeholder': _('Password')}))
     remember_me = forms.BooleanField(label='Remember me', required=False)
 
     def clean_email(self):
