8a107400c1af8cb37a6afcd5569b9d4e41cb1dec
richard <rich.290401@gmail.com>
2023-08-26 17:01:02 +0200

    WN-Tests: - Added core tests

--- core/UserFeedback/tests/test_user_feedback.py
+++ core/UserFeedback/tests/test_user_feedback.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from ..models import UserFeedback
 from ..factories import UserFeedbackFactory
+from ...User.factories import UserFactory
 
 
 class UserFeedbackTests(TestCase):
@@ -21,3 +22,25 @@ class UserFeedbackTests(TestCase):
         obj = UserFeedbackFactory.create()
         self.assertTrue(obj.email in str(obj))
         self.assertTrue(obj.email in obj.label)
+
+    def test_can_be_replied(self):
+        obj = UserFeedbackFactory.create(status=UserFeedback.Status.NEW)
+        self.assertTrue(obj.can_be_replied)
+
+        obj.status = UserFeedback.Status.COMMENTED
+        obj.save()
+        self.assertFalse(obj.can_be_replied)
+
+    def test_on_view_action(self):
+        obj = UserFeedbackFactory.create(status=UserFeedback.Status.NEW)
+        user = UserFactory.create()
+        obj.on_view_action(user=user)
+        self.assertEqual(obj.status, UserFeedback.Status.VIEWED)
+        self.assertEqual(obj.admin, user)
+
+    def test_on_reply_action(self):
+        obj = UserFeedbackFactory.create(status=UserFeedback.Status.NEW)
+        reply = UserFeedbackFactory.create(feedback=obj)
+        obj.on_reply_action(reply)
+        self.assertEqual(obj.status, UserFeedback.Status.COMMENTED)
+        self.assertEqual(obj.admin, reply.admin)
