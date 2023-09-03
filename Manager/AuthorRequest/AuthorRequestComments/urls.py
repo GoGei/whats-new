f552ef1be3dde888e75af75aacbae0ac8d7884e4
richard <rich.290401@gmail.com>
2023-09-03 16:00:25 +0200

    WN-64: - Applied UUID Ids to Post comments (drop tree comments)

--- Manager/AuthorRequest/AuthorRequestComments/urls.py
+++ Manager/AuthorRequest/AuthorRequestComments/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'<int:comment_id>/view/', views.author_request_comment_view, name='manager-author-request-comment-view'),
-    path(r'<int:comment_id>/edit/', views.author_request_comment_edit, name='manager-author-request-comment-edit'),
-    path(r'<int:comment_id>/archive/', views.author_request_comment_archive,
+    path(r'<uuid:comment_id>/view/', views.author_request_comment_view, name='manager-author-request-comment-view'),
+    path(r'<uuid:comment_id>/edit/', views.author_request_comment_edit, name='manager-author-request-comment-edit'),
+    path(r'<uuid:comment_id>/archive/', views.author_request_comment_archive,
          name='manager-author-request-comment-archive'),
-    path(r'<int:comment_id>/restore/', views.author_request_comment_restore,
+    path(r'<uuid:comment_id>/restore/', views.author_request_comment_restore,
          name='manager-author-request-comment-restore'),
 ]
